a6eb48b94e6f73e07af81e949fbc3fe41ba52ddd
Dmitri Kondramachine <dkondramachine@league.com>
2022-05-10 15:25:14 -0400

    --Update for compose

--- app/src/main/java/com/nortohol/todoauth0/MainActivity.kt
+++ app/src/main/java/com/nortohol/todoauth0/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.nortohol.todoauth0
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MyApp {
+                Greeting("Android")
+            }
+        }
     }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    Surface(color = Color.Yellow) {
+        content()
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello, $name",
+        modifier = Modifier
+            .padding(24.dp)
+    )
 }
